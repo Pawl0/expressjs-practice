07abcf8b8dede1af2de522d1210b7d32af1a2668
Paulo Santos <paulohsantosol@gmail.com>
2021-02-05 22:39:44 -0400

    Created different response types and set mustache with hogan-middleware

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const hoganMiddleware = require('hogan-middleware')
 const port = 3000
 
-const retObj = {
-    "key1": "value1",
-    "key2": "value2",
-}
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.engine('mustache', hoganMiddleware.__express)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,10 +16,19 @@ app.get('/test', (req, res) => {
     res.send('Teste!')
   })
 
-app.get('/obj', (req, res) => {
-    res.send(retObj);
+app.get('/json', (req, res) => {
+    const data = {
+        key1: "value1",
+        key2: "value2",
+        key3: "value3",
+    }
+    res.json(data);
+})
+
+app.get('/home', (req, res, next) => {
+    res.render('home', null)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`)
 })
