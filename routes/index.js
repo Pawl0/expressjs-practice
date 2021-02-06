f0e4e1f3e0de3fd24026934a0afaddcd072d2b9d
Paulo Santos <paulohsantosol@gmail.com>
2021-02-05 23:29:24 -0400

    Learn how to use parameters and queries

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,43 @@ router.get('/', (req, res) => {
       res.render('home', null)
   })
 
+  /*  This route redirects requests to Turbo360. */
+router.get('/redirect', (req, res) => {
+    res.redirect('https://www.turbo360.co/landing')
+  })
+  
+  // Assign a variable via parameter
+//   router.get('/:path', (req, res) => {
+//     const path = req.params.path
+  
+//     res.json({
+//       "data": path
+//     })
+//   })
+  
+  // Use 2 parameters
+  router.get('/:profile/:username', (req, res) => {
+    const profile = req.params.profile
+    const username = req.params.username
+  
+    res.json({
+      "profile": profile,
+      "username": username,
+    })
+  })
+
+  // Query example (/query?name=name&occupation=occupation)
+  router.get('/query', (req, res) => {
+    const name = req.query.name
+    const occupation = req.query.occupation
+
+    const data = {
+        name: name,
+        occupation
+    }
+    
+    res.render('profile', data)
+  })
+
+  
   module.exports = router
